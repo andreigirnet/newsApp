604e67c09a9e3f7b8edfc6cdab329e7d8b9dd43a
Your Name <you@example.com>
2024-07-17 12:16:14 +0300

    signle news

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <meta content="News" name="description">
 
     <!-- Favicon -->
-    <link href="{{asset('assets/img/icon.png')}}" rel="icon">
+    <link href="{{asset('assets/img/icon.png')}}" rel="icon" type="image/x-icon">
 
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap" rel="stylesheet">
