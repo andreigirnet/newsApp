713a7d62cd81ffcaadfbd26607a28cc707267035
Your Name <you@example.com>
2024-07-17 12:36:16 +0300

    signle news blog removessd

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -70,7 +70,7 @@ class NewsController extends Controller
         $news->hot_news = (int) $request['hotNews'];
         $news->save();
 
-        $newsUrl = url("/{$news->slug}");
+        $newsUrl = url("/singleNews/{$news->slug}");
         $newsUrl = str_replace("http://", "https://", $newsUrl);
         $sitemapPath = public_path('sitemap.xml');
 
